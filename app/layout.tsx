025b70c9f9ead7b406281e338c5fb35989329c9c
elizabthpazp <elizabthpazp695@gmail.com>
2025-03-28 20:57:04 -0600

    fix: set dark mode by default

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,7 +50,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="dark" suppressHydrationWarning>
       <head>
         <link
           rel="stylesheet"
