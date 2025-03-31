1b30fdf670327f2d6026c042f2a7273f8c4de4b8
elizabthpazp <elizabthpazp695@gmail.com>
2025-03-31 17:02:27 -0600

    fix: revert dark mode by default

--- components/contexts/theme-provider.tsx
+++ components/contexts/theme-provider.tsx
@@ -6,7 +6,7 @@ import { type ThemeProviderProps } from "next-themes/dist/types";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
-    <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem={false} {...props}>
+    <NextThemesProvider {...props}>
       {children}
     </NextThemesProvider>
   );
